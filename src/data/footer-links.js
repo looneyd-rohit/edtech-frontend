5e867ba1069f1609fb27147ba6cecb3c1f0922cd
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-06-30 21:24:41 +0530

    homepage and state slice done

---
+++ src/data/footer-links.js
@@ -0,0 +1,31 @@
+export const FooterLink2 = [
+  {
+    title: "Subjects",
+    links: [
+      { title: "Cybersecurity", link: "/cybersecurity" },
+      { title: "DevOps", link: "/devops" },
+      { title: "Game Development", link: "/game-development" },
+      { title: "Machine Learning", link: "/machine-learning" },
+      { title: "Web/Mobile Development", link: "/mobile-development" },
+    ],
+  },
+  {
+    title: "Languages",
+    links: [
+      { title: "Bash", link: "/bash" },
+      { title: "C++", link: "/c++" },
+      { title: "HTML & CSS", link: "/html-css" },
+      { title: "Java", link: "/java" },
+      { title: "JavaScript", link: "/javascript" },
+      { title: "Python", link: "/python" },
+      { title: "SQL", link: "/sql" },
+    ],
+  },
+  {
+    title: "Career building",
+    links: [
+      { title: "Career paths", link: "/career-paths" },
+      { title: "Jobs", link: "/jobs/apply" },
+    ]
+  }
+];
