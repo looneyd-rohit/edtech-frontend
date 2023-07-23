097c3ffff7bcfbac681a1cfb0337032e0100788c
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-07-23 11:49:32 +0530

    fixed buggy duration display

--- src/services/operations/courseDetailsAPI.js
+++ src/services/operations/courseDetailsAPI.js
@@ -272,6 +272,7 @@ export const fetchInstructorCourses = async (token) => {
         Authorization: `Bearer ${token}`,
       }
     )
+    // console.log(response)
     // console.log("INSTRUCTOR COURSES API RESPONSE............", response)
     if (!response?.data?.success) {
       throw new Error("Could Not Fetch Instructor Courses")
